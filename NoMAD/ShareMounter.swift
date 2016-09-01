8d220f2ebfd2731e5370b80d931bde22ea27fed0
mactroll <mactroll@mac.com>
2016-08-31 23:55:31 -0500

    merged

--- NoMAD/ShareMounter.swift
+++ NoMAD/ShareMounter.swift
@@ -6,7 +6,8 @@
 //  Copyright © 2016 Trusource Labs. All rights reserved.
 //
 
-// Thanks much to Kyle Crawshaw and ShareMounter
+// mad props to Kyle Crawshaw
+// since much of this is cribbed from Share Mounter
 
 import Foundation
 import NetFS
