66e55e4b907fde5a61c20fd844a4c0f9667fe531
mactroll <mactroll@mac.com>
2016-10-11 22:15:52 -0500

    fix for unwrapping a nil value for the LastCertificateExpiration date

--- NoMAD/NoMADMenuController.swift
+++ NoMAD/NoMADMenuController.swift
@@ -836,7 +836,11 @@ class NoMADMenuController: NSObject, LoginWindowDelegate, PasswordChangeDelegate
             dateFormatter.dateStyle = .MediumStyle
             dateFormatter.timeStyle = .ShortStyle
             
-            NoMADMenuGetCertificateDate.title = dateFormatter.stringFromDate(defaults.objectForKey("LastCertificateExpiration") as! NSDate) ?? "No certs"
+            if let expireDate = defaults.objectForKey("LastCertificateExpiration") as? NSDate {
+                NoMADMenuGetCertificateDate.title = dateFormatter.stringFromDate(expireDate)
+            } else {
+                NoMADMenuGetCertificateDate.title = "No Certs"
+            }
             
         } else {
             myLogger.logit(1, message:"Time between system checks is too short, delaying")
