73415663de7ec46c9815015bb4c59063298ebc4a
Tom Nook <tomnook@Slenderman.local>
2016-10-29 15:06:27 -0400

    Converted ShareMounter.swift to Swift 3 syntax.

--- NoMAD/ShareMounter.swift
+++ NoMAD/ShareMounter.swift
@@ -28,7 +28,7 @@ class ShareMounter {
         // read in the preference files
         
 
-        if defaults.integerForKey("ShowHome") == 1 {
+        if defaults.integer(forKey: "ShowHome") == 1 {
         //NSLog("Looking for ShareMounter files")
         //prefs = try! ["/Library/Preferences/ShareMounter.plist", NSHomeDirectory() + "/Library/Preferences/ShareMounter.plist"]
         /*
@@ -44,7 +44,7 @@ class ShareMounter {
     
     func mount() {
         
-        let myGroups = defaults.arrayForKey("Groups")
+        let myGroups = defaults.array(forKey: "Groups")
     
         for share in all_shares {
             for group in myGroups! {
@@ -63,10 +63,10 @@ class ShareMounter {
             let myDictionary = NSDictionary.init(contentsOfFile: place)
             
             if myDictionary != nil {
-                let shares = (myDictionary?.objectForKey("network_shares"))! as! [AnyObject]
+                let shares = (myDictionary?.object(forKey: "network_shares"))! as! [AnyObject]
                 
                 for i in shares {
-                    let currentShare = share_info(groups: i.objectForKey("groups")! as! Array, share_url: i.objectForKey("share_url")! as! String, title: i.objectForKey("title")! as! String)
+                    let currentShare = share_info(groups: i.object(forKey: "groups")! as! Array, share_url: i.object(forKey: "share_url")! as! String, title: i.object(forKey: "title")! as! String)
                     all_shares.append(currentShare)
                 }
             }
@@ -86,42 +86,48 @@ class ShareMounter {
         return dict
     }
     
-    func asyncMountShare(serverAddress: String) {
+    func asyncMountShare(_ serverAddress: String) {
         
-        let escapedAddress = serverAddress.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
-        let shareAddress = NSURL(string: escapedAddress!)!
+        let escapedAddress = serverAddress.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)
+        let shareAddress = URL(string: escapedAddress!)!
 
         // TODO: ensure the URL is reachable before attempting to mount
         
         //let open_options : CFMutableDictionary = openOptionsDict()
         let mount_options : CFMutableDictionary = mountOptionsDict()
         
-        var requestID: AsyncRequestID = nil
-        let queue = dispatch_get_main_queue()
+        var requestID: AsyncRequestID? = nil
+        let queue = DispatchQueue.main
         
-        NetFSMountURLAsync(shareAddress, nil, defaults.stringForKey("userPrincipal")!, nil, nil, mount_options, &requestID, queue)
-        {(stat:Int32,  requestID:AsyncRequestID,  mountpoints:CFArray!) -> Void in
-            print("msg: \(stat) mountpoint: \(mountpoints)")
+        NetFSMountURLAsync(shareAddress as CFURL!,
+                           nil,
+                           defaults.string(forKey: "userPrincipal")! as CFString!,
+                           nil,
+                           nil,
+                           mount_options,
+                           &requestID,
+                           queue)
+       {(stat:Int32, requestID:AsyncRequestID?, mountpoints:CFArray?) -> Void in
+        myLogger.logit(0, message: "Mounted \(mountpoints)")
         }
-    }
+
     
     func getNetworkShares() {
-    let fm = NSFileManager.defaultManager()
+    let fm = FileManager.default
     
-    let myShares = fm.mountedVolumeURLsIncludingResourceValuesForKeys(nil, options: NSVolumeEnumerationOptions.SkipHiddenVolumes)
+    let myShares = fm.mountedVolumeURLs(includingResourceValuesForKeys: nil, options: FileManager.VolumeEnumerationOptions.skipHiddenVolumes)
     
     for share in myShares! {
     
     var myDes: NSString? = nil
     var myType: NSString? = nil
     
-    ws.getFileSystemInfoForPath(share.path!, isRemovable: nil, isWritable: nil, isUnmountable: nil, description: &myDes, type: &myType)
+    ws.getFileSystemInfo(forPath: share.path, isRemovable: nil, isWritable: nil, isUnmountable: nil, description: &myDes, type: &myType)
     print(myType!)
     if myType! == "smbfs" {
-    print("Volume: " + share.path! + " is an SMB network volume.")
+    print("Volume: " + share.path + " is an SMB network volume.")
     }
 }
     }
 }
-
-
+}
