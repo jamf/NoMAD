702d7666db6c5dc28e6c7386102f266d9a8c2a1d
mactroll <mactroll@mac.com>
2016-10-31 09:54:34 -0500

    Updated Dictionary typing.

--- NoMAD/Localizator.swift
+++ NoMAD/Localizator.swift
@@ -8,22 +8,24 @@
 
 import Foundation
 
-class Localizator {
-	
-	static let sharedInstance = Localizator()
-	
-	lazy var localizableDictionary: NSDictionary! = {
-		if let path = Bundle.main.path(forResource: "Languages", ofType: "plist") {
-			return NSDictionary(contentsOfFile: path)
-		}
-		fatalError("Localizable file NOT found")
-	}()
-	
-	func translate(_ string: String) -> String {
-		guard let localizedString = (localizableDictionary.value(forKey: string) as AnyObject).value(forKey: "value") as? String else {
-			assertionFailure("Missing translation for: \(string)")
-			return ""
-		}
-		return localizedString
-	}
+import Foundation
+
+class Localizator : NSObject {
+    
+    static let sharedInstance = Localizator()
+    
+    lazy var localizableDictionary: [String:AnyObject] = {
+        if let path = Bundle.main.path(forResource: "Languages", ofType: "plist") {
+            return NSDictionary(contentsOfFile: path) as! [String : AnyObject]
+        }
+        fatalError("Localizable file NOT found")
+    }()
+    
+    func translate(_ string: String) -> String {
+        guard let localizedString = localizableDictionary[string]?["value"] else {
+            assertionFailure("Missing translation for: \(string)")
+            return ""
+        }
+        return localizedString as! String
+    }
 }
