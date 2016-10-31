70598c9c857f8cc991a19e95284262a3672d6e53
mactroll <mactroll@mac.com>
2016-10-31 11:57:05 -0500

    removed double import of Foundation

--- NoMAD/Localizator.swift
+++ NoMAD/Localizator.swift
@@ -8,19 +8,17 @@
 
 import Foundation
 
-import Foundation
-
 class Localizator : NSObject {
-    
+
     static let sharedInstance = Localizator()
-    
+
     lazy var localizableDictionary: [String:AnyObject] = {
         if let path = Bundle.main.path(forResource: "Languages", ofType: "plist") {
             return NSDictionary(contentsOfFile: path) as! [String : AnyObject]
         }
         fatalError("Localizable file NOT found")
     }()
-    
+
     func translate(_ string: String) -> String {
         guard let localizedString = localizableDictionary[string]?["value"] else {
             assertionFailure("Missing translation for: \(string)")
