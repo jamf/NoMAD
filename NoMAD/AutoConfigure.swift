b62ca19d1b4b2d41e3297e320cda135051b454c6
Tom Nook <tomnook@slenderman.attlocal.net>
2016-11-06 15:18:26 -0500

    Updated AutoConfigure.swift with project whitespace settings.

--- NoMAD/AutoConfigure.swift
+++ NoMAD/AutoConfigure.swift
@@ -8,59 +8,59 @@
 
 import Foundation
 
-    // site information
-    
-    public func setDefaults() {
-    
+// site information
+
+public func setDefaults() {
+
     // do we have an AutoConfigure setting?
-        
+
     if let autoConfigure = defaults.string(forKey: "AutoConfigure") {
         switch autoConfigure {
-            case "TSL":
-                defaults.set("trusourcelabs.com", forKey: "ADDomain")
-                defaults.set("TRUSOURCELABS.COM", forKey: "KerberosRealm")
-                //defaults.setObject("jupiter.trusourcelabs.com", forKey: "InternalSite")
-                //defaults.setObject("192.168.32.43", forKey: "InternalSiteIP")
-                defaults.set(3, forKey: "Verbose")
-                defaults.set("", forKey: "userCommandHotKey1")
-                defaults.set("", forKey: "userCommandName1")
-                defaults.set("", forKey: "userCommandTask1")
-                defaults.set(7200, forKey: "secondsToRenew")
-                defaults.set(1, forKey: "RenewTickets")
-                defaults.set("", forKey: "AutoConfigure")
-           
-            case "JODA":
-                defaults.set("jodapro.com", forKey: "ADDomain")
-                defaults.set("JODAPRO.COM", forKey: "KerberosRealm")
-                //defaults.setObject("in-or-out.jodapro.com", forKey: "InternalSite")
-                //defaults.setObject("1.1.1.1", forKey: "InternalSiteIP")
-                defaults.set("2k12.jodapro.com", forKey: "x509CA")
-                defaults.set("User Auth", forKey: "Template")
-                defaults.set(3, forKey: "Verbose")
-                defaults.set("", forKey: "userCommandHotKey1")
-                defaults.set("", forKey: "userCommandName1")
-                defaults.set("", forKey: "userCommandTask1")
-                defaults.set(7200, forKey: "secondsToRenew")
-                defaults.set(1, forKey: "RenewTickets")
-                defaults.set("", forKey: "AutoConfigure")
-            
-            default:
-                // see if we're on AD
-                getADSettings()
-                break
-            }
-        }
-        
-        if defaults.bool(forKey: "LoginItem") {
-            //TODO: Test this to make sure it actually does what I think it will. This should return the value of the key if found, otherwise false.
-                addToLoginItems()
+        case "TSL":
+            defaults.set("trusourcelabs.com", forKey: "ADDomain")
+            defaults.set("TRUSOURCELABS.COM", forKey: "KerberosRealm")
+            //defaults.setObject("jupiter.trusourcelabs.com", forKey: "InternalSite")
+            //defaults.setObject("192.168.32.43", forKey: "InternalSiteIP")
+            defaults.set(3, forKey: "Verbose")
+            defaults.set("", forKey: "userCommandHotKey1")
+            defaults.set("", forKey: "userCommandName1")
+            defaults.set("", forKey: "userCommandTask1")
+            defaults.set(7200, forKey: "secondsToRenew")
+            defaults.set(1, forKey: "RenewTickets")
+            defaults.set("", forKey: "AutoConfigure")
+
+        case "JODA":
+            defaults.set("jodapro.com", forKey: "ADDomain")
+            defaults.set("JODAPRO.COM", forKey: "KerberosRealm")
+            //defaults.setObject("in-or-out.jodapro.com", forKey: "InternalSite")
+            //defaults.setObject("1.1.1.1", forKey: "InternalSiteIP")
+            defaults.set("2k12.jodapro.com", forKey: "x509CA")
+            defaults.set("User Auth", forKey: "Template")
+            defaults.set(3, forKey: "Verbose")
+            defaults.set("", forKey: "userCommandHotKey1")
+            defaults.set("", forKey: "userCommandName1")
+            defaults.set("", forKey: "userCommandTask1")
+            defaults.set(7200, forKey: "secondsToRenew")
+            defaults.set(1, forKey: "RenewTickets")
+            defaults.set("", forKey: "AutoConfigure")
+
+        default:
+            // see if we're on AD
+            getADSettings()
+            break
         }
     }
 
+    if defaults.bool(forKey: "LoginItem") {
+        //TODO: Test this to make sure it actually does what I think it will. This should return the value of the key if found, otherwise false.
+        addToLoginItems()
+    }
+}
+
 private func getADSettings() {
-    
+
     let myADConfig = cliTask("/usr/sbin/dsconfigad -show").components(separatedBy: "\n")
-    
+
     if myADConfig.count > 0 {
         for line in myADConfig {
             if line.contains("Active Directory Domain") {
@@ -79,64 +79,64 @@ private func getADSettings() {
             }
         }
     }
-    
+
 }
 
 private func addToLoginItems() {
-    
+
     NSLog("Creating LaunchAgent.")
-    
+
     // see if the folder exists
-    
+
     let myFileManager = FileManager()
     let myLaunchAgentFolder = NSHomeDirectory() + "/Library/LaunchAgents/"
-    
+
     if ( myFileManager.fileExists( atPath: myLaunchAgentFolder, isDirectory: nil)) {
-        
+
     }
-    
+
     else {
         do {
             NSLog("Creating LaunchAgent folder.")
-            
+
             try myFileManager.createDirectory(atPath: myLaunchAgentFolder, withIntermediateDirectories: true, attributes: nil)
         }
-        
+
         catch {
             NSLog("Can't create LaunchAgent folder.")
             return
         }
     }
-    
+
     // find the current app path and create a launch agent
-    
+
     let myBinaryPath = Bundle.main.bundlePath + "/Contents/MacOS/NoMAD"
     let myLaunchAgentPath = NSHomeDirectory() + "/Library/LaunchAgents/com.trusourcelabs.NoMAD.plist"
-    
+
     // build the launch agent plist file
-    
+
     let data = NSMutableDictionary()
-    
+
     data.setObject(true, forKey: "KeepAlive" as NSCopying)
     data.setObject("com.trusourcelabs.NoMAD", forKey: "Label" as NSCopying)
     data.setObject(true, forKey: "RunAtLoad" as NSCopying)
     data.setObject([ myBinaryPath ], forKey: "ProgramArguments" as NSCopying)
     data.setObject(["Aqua"], forKey: "LimitLoadToSessionType" as NSCopying)
-    
+
     data.write( toFile: myLaunchAgentPath, atomically: true)
-    
+
     // clear the flag
-    
+
     defaults.set(false, forKey: "LoginItem")
-    
+
     // in honor of @macmule
     /*
-    NSTask.launchedTaskWithLaunchPath(
-        "/usr/bin/osascript",
-        arguments: [
-            "-e",
-            "tell application \"System Events\" to make login item at end with properties {path:\"" + myAppPath + "\", hidden:false, name:\"NoMad Password Monitor\"}"
-        ]
-    )
- */
+     NSTask.launchedTaskWithLaunchPath(
+     "/usr/bin/osascript",
+     arguments: [
+     "-e",
+     "tell application \"System Events\" to make login item at end with properties {path:\"" + myAppPath + "\", hidden:false, name:\"NoMad Password Monitor\"}"
+     ]
+     )
+     */
 }
