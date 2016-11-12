b5bc9bf4190c8d6abb4481fa87a2abaf58323b10
Tom Nook <tomnook@Slenderman.local>
2016-11-12 16:45:18 -0500

    Normalized Preference key names. Added constants for key names in PreferenceConstants.swift

---
+++ NoMAD/PreferenceConstants.swift
@@ -0,0 +1,39 @@
+//
+//  PreferenceConstants.swift
+//  NoMAD
+//
+//  Created by Tom Nook on 11/12/16.
+//  Copyright © 2016 Trusource Labs. All rights reserved.
+//
+
+let ADDomain = "ADDomain"
+let DisplayName = "DisplayName"
+let ExpeditedLookup = "ExpeditedLookup"
+let GetHelpType = "GetHelpType"
+let GetHelpOptions = "GetHelpOptions"
+let Groups = "Groups"
+let HidePrefs = "HidePrefs"
+let KerberosRealm = "KerberosRealm"
+let LastCertificateExpiration = "LastCertificateExpiration"
+let LoginComamnd = "LoginComamnd"
+let LDAPServerList = "LDAPServerList"
+let LocalPasswordSync = "LocalPasswordSync"
+let LastUser = "LastUser"
+let LastPasswordWarning = "LastPasswordWarning"
+let PasswordExpireAlertTime = "PasswordExpireAlertTime"
+let RenewTickets = "RenewTickets"
+let ShowHome = "ShowHome"
+let SecondsToRenew = "SecondsToRenew"
+let SignInCommand = "SignInCommand"
+let StateChangeAction = "StateChangeAction"
+let Template = "Template"
+let UserPrincipal = "UserPrincipal"
+let UserPasswordExpireDate = "UserPasswordExpireDate"
+let UserCommandTask1 = "UserCommandTask1"
+let UserCommandName1 = "UserCommandName1"
+let UserCommandHotKey1 = "UserCommandHotKey1"
+let UserPasswordSetDates = "UserPasswordSetDates"
+let UseKeychain = "UseKeychain"
+let UserAging = "UserAging"
+let Verbose = "Verbose"
+let x509CA = "x509CA"
