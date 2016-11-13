053ca42b3f735c1833a561f44c8b2ddd17cc360e
Tom Nook <tomnook@Slenderman.local>
2016-11-13 15:23:47 -0500

    Cleaned up AppDelegate.swift.
    Removed dead code and whitespace.

--- NoMAD/AppDelegate.swift
+++ NoMAD/AppDelegate.swift
@@ -8,7 +8,6 @@
 
 import Cocoa
 import SystemConfiguration
-import Foundation
 
 let notificationKey = Notification(name: Notification.Name(rawValue: "updateNow"), object: nil)
 let notificationCenter = NotificationCenter.default
@@ -18,29 +17,18 @@ let notificationQueue = NotificationQueue.default
 
 class AppDelegate: NSObject, NSApplicationDelegate, NSUserNotificationCenterDelegate {
 
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-
         myLogger.logit(.base, message:"---NoMAD Initialized---")
 
         let defaultPreferences = NSDictionary(contentsOf: Bundle.main.url(forResource: "DefaultPreferences", withExtension: "plist")!)
-        UserDefaults.standard.register(defaults: defaultPreferences as! [String : Any])
-
-        //NSUserNotificationCenter.defaultUserNotificationCenter().delegate = self
-
-        func sendUpdateRequest() {
-
-            NSLog("State change, checking things.")
-            notificationQueue.enqueue(notificationKey, postingStyle: .now, coalesceMask: .onName, forModes: nil)
-        }
+        defaults.register(defaults: defaultPreferences as! [String : Any])
 
         let changed: SCDynamicStoreCallBack = {SCDynamicStore,_,_ in
-
-            NSLog("State change, checking things.")
+            myLogger.logit(.base, message: "State change, checking things.")
             notificationQueue.enqueue(notificationKey, postingStyle: .now, coalesceMask: .onName, forModes: nil)
 
-            if  ( defaults.string(forKey: Preferences.stateChangeAction) != nil ) {
-                NSLog("Firing State Change Action")
+            if (defaults.string(forKey: Preferences.stateChangeAction) != nil ) {
+                myLogger.logit(.base, message: "Firing State Change Action")
                 cliTask(defaults.string(forKey: Preferences.stateChangeAction)! + " &")
             }
         }
@@ -49,12 +37,9 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSUserNotificationCenterDele
         let dcAddress = withUnsafeMutablePointer(to: &dynamicContext, {UnsafeMutablePointer<SCDynamicStoreContext>($0)})
 
         // set a 15 minute timer to update everything
-
         Timer.scheduledTimer(timeInterval: 900, target: self, selector: #selector(sendUpdateMessage), userInfo: nil, repeats: true)
 
-
         if let dynamicStore = SCDynamicStoreCreate(kCFAllocatorDefault, "io.fti.networkconfigurationchanged" as CFString, changed, dcAddress){
-
             let keys: [CFString] = ["State:/Network/Global/IPv4" as CFString]
             let keysArray = keys as CFArray
 
@@ -63,21 +48,17 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSUserNotificationCenterDele
             let loop = SCDynamicStoreCreateRunLoopSource(kCFAllocatorDefault, dynamicStore, 0)
             CFRunLoopAddSource(CFRunLoopGetCurrent(), loop, CFRunLoopMode.defaultMode)
 
-
             CFRunLoopRun()
         }
-
         awakeFromNib()
-
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+        defaults.synchronize()
     }
 
-    func sendUpdateMessage() -> Void {
-        NSLog("It's been a while, checking things.")
+    func sendUpdateMessage() {
+        myLogger.logit(.base, message: "It's been a while, checking things.")
         notificationQueue.enqueue(notificationKey, postingStyle: .now, coalesceMask: .onName, forModes: nil)
     }
-
 }
