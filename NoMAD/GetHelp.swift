ec6da46f0ff82b48823f359e4e54a72eaaa9f9e5
Tom Nook <tomnook@icloud.com>
2016-11-26 13:17:22 -0500

    Added openDefaultHelpURL() to remove code duplication. Switched to NSWorkspace launch methods for apps.

--- NoMAD/GetHelp.swift
+++ NoMAD/GetHelp.swift
@@ -18,34 +18,41 @@ class GetHelp {
         if let getHelpType = defaults.string(forKey: Preferences.getHelpType),
             let getHelpOptions = defaults.string(forKey: Preferences.getHelpOptions) {
 
-            if getHelpType != "" && getHelpOptions != "" {
+            if getHelpType.characters.count > 0 && getHelpOptions.characters.count > 0 {
                 switch getHelpType {
                 case "Bomgar":
                     if let myURL = subVariables(getHelpOptions) {
                         cliTask("curl -o /tmp/BomgarClient " + myURL )
                         cliTaskNoTerm("/usr/bin/unzip -o -d /tmp /tmp/BomgarClient")
-                        cliTask("/usr/bin/open /tmp/Bomgar/Double-Click\\ To\\ Start\\ Support\\ Session.app")
+                        NSWorkspace.shared().launchApplication("/tmp/Bomgar/Double-Click\\ To\\ Start\\ Support\\ Session.app")
                     }
                 case "URL":
-                    if let myURL = subVariables(getHelpOptions) {
-                        let url = URL(string: myURL)
-                        NSWorkspace.shared().open(url!)
-                    }
+                        guard let url = URL(string: getHelpOptions) else {
+                            myLogger.logit(.base, message: "Could not create help URL.")
+                            break
+                        }
+                        NSWorkspace.shared().open(url)
                 case "Path":
-                    cliTask("/usr/bin/open " + getHelpOptions.replacingOccurrences(of: " ", with: "\\ ") )
+                    cliTask(getHelpOptions.replacingOccurrences(of: " ", with: "\\ ") )
                 case "App":
                     NSWorkspace.shared().launchApplication(getHelpOptions)
                 default:
                     myLogger.logit(.info, message: "Invalid getHelpType or getHelpOptions, defaulting to www.apple.com/support")
-                    let url = URL(string: "http://www.apple.com/support")!
-                    NSWorkspace.shared().open(url)
+                    openDefaultHelpURL()
                 }
             } else {
                 myLogger.logit(.info, message: "No help options set, defaulting to www.apple.com/support")
-                let url = URL(string: "http://www.apple.com/support")!
-                NSWorkspace.shared().open(url)
+                openDefaultHelpURL()
             }
         }
+    }
+
+    fileprivate func openDefaultHelpURL() {
+        guard let url = URL(string: "http://www.apple.com/support") else {
+            myLogger.logit(.base, message: "Could not create default help URL.")
+            return
+        }
+        NSWorkspace.shared().open(url)
     }
 
     fileprivate func subVariables(_ url: String) -> String? {
@@ -66,30 +73,5 @@ class GetHelp {
         createdURL = createdURL.replacingOccurrences(of: "<<shortname>>", with: shortName)
 
         return createdURL
-        /*
-         if let domain = defaults.stringForKey("ADDomain") {
-         if let fullName = defaults.stringForKey("displayName")!.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet()) {
-         let shortName = defaults.stringForKey("UserShortName") ?? ""
-         let serial = getSerial()
-
-         let subURL = url.stringByReplacingOccurrencesOfString("<<domain>>", withString: domain)
-         let subURL1 = subURL.stringByReplacingOccurrencesOfString("<<fullname>>", withString: fullName)
-         let subURL2 = subURL1.stringByReplacingOccurrencesOfString("<<serial>>", withString: serial)
-         let subURL3 = subURL2.stringByReplacingOccurrencesOfString("<<shortname>>", withString: shortName)
-
-         return subURL3
-         }
-
-         else {
-         print ("displayName key failure")
-
-         return ""
-         }
-         }
-
-         print("ADDomain key failure")
-         
-         return ""
-         */
     }
 }
