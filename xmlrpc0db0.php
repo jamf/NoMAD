366fe4bd6c9e41d981f1caaa7369c3111d594788
Josh Wisenbaker <josh.wisenbaker@jamf.com>
2022-01-11 14:32:35 -0500

    gh-pages nomad site import

---
+++ xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://nomad.menu</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://nomad.menu/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://nomad.menu/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://nomad.menu/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://nomad.menu/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://nomad.menu/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
